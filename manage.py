15736e1d2f1803bdf819ea3f05277c2be0ad3184
brandon.mino <brandon.mino@uconn.edu>
2021-08-31 21:22:46 -0400

    fix: remove bug code from manage

--- manage.py
+++ manage.py
@@ -6,11 +6,6 @@ from app.main.controllers.predict_controller import PredictController
 app = Flask(__name__)
 api = Api(app)
 
-class HelloWorld(Resource):
-    def get(self):
-        return {'hello': 'world'}
-
-api.add_resource(HelloWorld, '/')
 api.add_resource(PredictController, '/predict')
 api.init_app(app)
 
